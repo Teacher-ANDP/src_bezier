560f13a75547d5493a9c75b86bc819fa7c06c1d8
Sukkertoppen-ANDP <andp@nextkbh.dk>
2025-02-04 23:46:41 +0100

    De Casteljau version tilføjet

--- sketch.js
+++ sketch.js
@@ -86,4 +86,36 @@ function mouseReleased(){
   } 
 }
 
-let k=0;
+/*
+/////////// version til et virkårligt antal punkter
+let points = [ [0, 128], [128, 0], [256, 0], [384, 128] ]
+function drawDecasteljau(points){
+  for(let i = 0; i < 1; i+=0.001){
+    let ps = crlPtReduceDeCasteljau (points, i)
+    circle(ps[ps.length-1][0][0],ps[ps.length-1][0][1],10)
+  }
+}
+
+//https://en.wikipedia.org/wiki/De_Casteljau%27s_algorithm 
+function crlPtReduceDeCasteljau(points, t) {
+    let retArr = [ points.slice () ];
+	while (points.length > 1) {
+        let midpoints = [];
+		for (let i = 0; i+1 < points.length; ++i) {
+			let ax = points[i][0];
+			let ay = points[i][1];
+			let bx = points[i+1][0];
+			let by = points[i+1][1];
+            // a * (1-t) + b * t = a + (b - a) * t
+			midpoints.push([
+				ax + (bx - ax) * t,
+				ay + (by - ay) * t,
+			]);
+		}
+        retArr.push (midpoints)
+		points = midpoints;
+	}
+	return retArr;
+}
+////////////////
+*/
